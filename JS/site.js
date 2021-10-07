4312480cf15638db198ca91ab665542fd196691f
Carl Mclamb <ctmclamb@gmail.com>
2021-10-07 10:17:11 -0400

    added more

--- JS/site.js
+++ JS/site.js
@@ -13,4 +13,31 @@ function getValue() {
 //reverse the string 
 //aka logic function 
 
-function reverse
+function reverseString(userString) {
+
+    //last position in an array name name.length -1
+
+
+    let revString =[];
+
+    //reverse the string using a for loop 
+
+
+    for (let index = userString.length - 1; index >= 0; index--) {
+        const element = userString[index];
+        
+    }
+
+    return revString;
+
+}
+
+//display the string in reverse 
+
+function displayString(revString) {
+
+
+    //write the message to the page 
+    document.getElementById('msg').innerHTML = ` ${userString}  ${revString}`;
+
+}
