c1725971492321270b9f6c61f62a4ef08593c560
Carl Mclamb <ctmclamb@gmail.com>
2021-10-07 00:13:44 -0400

    added to js file

--- JS/site.js
+++ JS/site.js
@@ -1,2 +1,16 @@
 //Get value from the UI 
+//aka controller function
 
+function getValue() {
+
+    document.getElementById("alert").classList("invisible");
+    let userString = document.getElementById("userString").value;
+    let revString = reverseString(userString);
+    displayString(revString);
+}
+
+
+//reverse the string 
+//aka logic function 
+
+function reverse
